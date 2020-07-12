bb5b8ce72f93bf4658ff2f01e24af17f1bbd1e5a
AlanHsu123 <o1234518@gmail.com>
2020-07-12 16:19:53 +0800

    Add user model & list item

---
+++ app/src/main/java/com/example/githubuserlist/model/User.kt
@@ -0,0 +1,12 @@
+package com.example.githubuserlist.model
+
+class User{
+    //self_img url
+    val avatare_url: String = ""
+    val login_id: String = ""
+    val site_admin: Boolean = false
+    val name: String = ""
+    val location: String = ""
+    val blog: String = ""
+    val bio: String = ""
+}
